e979ea6c0decfd2484270c911def064f3effdb3d
Aldis Sarja <aldis.sarja@gmail.com>
2022-12-25 16:44:26 +0200

    Sorting and caching

--- app/Services/GetAllBooksService.php
+++ app/Services/GetAllBooksService.php
@@ -8,26 +8,19 @@ class GetAllBooksService extends BookService
 {
     public function execute(): Collection
     {
-        $books = $this->bookRepository->getAllBooks();
-        $books = $books->map(function ($book) {
-            return $this->remapBookData($book);
-        });
-        return $books->sort(function ($book1, $book2) {
-            if ($book1->getTakenInCurrentMonth() === $book2->getTakenInCurrentMonth()) {
-                if ($book1->getTotalTaken() === $book2->getTotalTaken()) {
-                    return 0;
-                }
-                return ($book1->getTotalTaken() < $book2->getTotalTaken()) ? 1 : -1;
-            }
-            return ($book1->getTakenInCurrentMonth() < $book2->getTakenInCurrentMonth()) ? 1 : -1;
-        });
-
-        return cache()->remeber("AllBooks", self::CACHE_TIME, function () {
-            $books = $this->bookRepository->getAllBooks();
-            $books = $books->map(function ($book) {
+        return cache()->remember("AllBooks", self::CACHE_TIME, function () {
+            $books = $this->bookRepository->getAllBooks()->map(function ($book) {
                 return $this->remapBookData($book);
             });
-            return $books;
+            return $books->sort(function ($book1, $book2) {
+                if ($book1->getTakenInCurrentMonth() === $book2->getTakenInCurrentMonth()) {
+                    if ($book1->getTotalTaken() === $book2->getTotalTaken()) {
+                        return 0;
+                    }
+                    return ($book1->getTotalTaken() < $book2->getTotalTaken()) ? 1 : -1;
+                }
+                return ($book1->getTakenInCurrentMonth() < $book2->getTakenInCurrentMonth()) ? 1 : -1;
+            });
         });
     }
 }
